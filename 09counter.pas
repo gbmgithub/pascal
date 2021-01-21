7116016804db85ac5e0948d38f5f272eb9f51a41
GBM <72363278+gbmgithub@users.noreply.github.com>
2021-01-21 06:30:38 -0300

    Update 09counter.pas

--- 09counter.pas
+++ 09counter.pas
@@ -1,5 +1,7 @@
 Program 09 counter ;
+
 var sequencia: integer;    //contagem sempre integer ou char
+
 Begin
 	writeln ('Este programa mostra todos os números inteiros entre um e dez, incluindo estes.');
 	writeln ('Pressione "Enter" para dar prosseguimento');
