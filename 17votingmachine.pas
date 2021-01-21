d0742152e1aedcefe3b367113facae67c5c4c867
GBM <72363278+gbmgithub@users.noreply.github.com>
2021-01-21 06:48:28 -0300

    Update 17votingmachine.pas

--- 17votingmachine.pas
+++ 17votingmachine.pas
@@ -6,7 +6,7 @@ var tota,totb,totc,totbr,toti,totv:integer;
 Begin
 	write('Digite seu voto => ');
 	readln(voto);
-	voto:=upcase(voto);//upcase transforma minúsculas em maiúsculas
+	voto:=upcase(voto);//upcase transforma minúsculas em maiúsculas de forma a padronizar o conteúdo
 	while(voto <> '*') do
 		begin
 			totv:=totv+1;
