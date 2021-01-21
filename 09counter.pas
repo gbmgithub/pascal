d13ba12120c6f274a7086611fab5c09e9756c4fa
GBM <72363278+gbmgithub@users.noreply.github.com>
2021-01-21 06:26:55 -0300

    Update and rename N lista01 ex9 GRR20204490.pas to 09counter.pas

--- 09counter.pas
+++ 09counter.pas
@@ -1,7 +1,7 @@
-Program exer09 ;
+Program 09 counter ;
 var sequencia: integer;    //contagem sempre integer ou char
 Begin
-	writeln ('Este programa mostra todos os números inteiros entre um e dez, incluindo estes.');
+	writeln ('Este programa mostra todos os nÃºmeros inteiros entre um e dez, incluindo estes.');
 	writeln ('Pressione "Enter" para dar prosseguimento');
 	readln;
 	for sequencia:= 1 to 10 do    //valor inicial e valorfinal
