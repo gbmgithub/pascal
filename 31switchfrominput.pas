3f32bf3302d744662d86735c0d08f37b36ebf446
GBM <72363278+gbmgithub@users.noreply.github.com>
2021-01-21 06:50:48 -0300

    Update 31switchfrominput.pas

--- 31switchfrominput.pas
+++ 31switchfrominput.pas
@@ -1,7 +1,7 @@
 Program 31 switchfrominput ;
 
 var input:integer;
-		key:boolean;
+	key:boolean;
 Begin
 	key:=true;
 	writeln('Este programa lê números inteiros e verifica-os se são o número 5000, após, muda um switch e printa as leituras até que se digite 5000 novamente ou 5555 para encerrar');
@@ -13,4 +13,5 @@ Begin
 			writeln(input);
 	until (input=5555);
 	writeln ('O programa leu o valor 5555 e será encerrado.');
+	
 End.
