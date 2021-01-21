8fac5eb55ca85ce404a3b590a3da42d098a4bc49
GBM <72363278+gbmgithub@users.noreply.github.com>
2021-01-21 06:28:13 -0300

    Update and rename N02L1GUSTAVO_MICHEL.pas to 02seconddegreeequation.pas

--- 02seconddegreeequation.pas
+++ 02seconddegreeequation.pas
@@ -1,26 +1,26 @@
-Program exer02 ;
+Program 02 2nddegreeequation ;
 label 1,2,3,4,5,6,7,8,9,10;
 var a,b,c,delta,x1,x2,raiz:real;
 Begin {1}
-	writeln('Este programa calcula equações de segundo grau. ');
-	write('Digite o valor de "a" de sua equação: ');
+	writeln('Este programa calcula equaÃ§Ãµes de segundo grau. ');
+	write('Digite o valor de "a" de sua equaÃ§Ã£o: ');
   readln(a);
   if a<>0 then
   	begin {2}
-			write('Digite o valor de "b" de sua equação: ');
+			write('Digite o valor de "b" de sua equaÃ§Ã£o: ');
 			readln(b);
-			write('Digite o valor de "c" de sua equação: ');
+			write('Digite o valor de "c" de sua equaÃ§Ã£o: ');
 			readln(c); 
 	    delta:=b*b-4*a*c;
 	    if delta<0 then
 	    	begin {3}
-		    	write('Sua equação possui somente uma raíz. Logo, ');
+		    	write('Sua equaÃ§Ã£o possui somente uma raÃ­z. Logo, ');
 		    	x1:=-b/(2*a);
 		    	writeln('x1=x2= ', x1:2:2);
 		    end {3}
 		  else
 		  	begin {4}
-		  		write ('Sua equação possui raízes distintas. Sendo ');
+		  		write ('Sua equaÃ§Ã£o possui raÃ­zes distintas. Sendo ');
 		  		raiz:=sqrt(delta);
 		  		x1:=(-b+raiz)/(2*a);
 		      x2:=(-b-raiz)/(2*a);
@@ -29,6 +29,6 @@ Begin {1}
 		end {2}
 	else
 		begin
-			write('O valor de "a" precisa ser diferente de zero para ser uma equação de segundo grau.');
+			write('O valor de "a" precisa ser diferente de zero para ser uma equaÃ§Ã£o de segundo grau.');
 		end
 End.
